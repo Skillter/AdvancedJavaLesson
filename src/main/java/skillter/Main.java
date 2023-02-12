7d175c779da9993b97e81c091f50766a8787bd29
Skillter <57605954+Skillter@users.noreply.github.com>
2023-02-12 12:14:31 +0100

    Update Main.java

--- src/main/java/skillter/Main.java
+++ src/main/java/skillter/Main.java
@@ -1,7 +1,15 @@
 package skillter;
 
+import skillter.advanceddatastructuers.HashmapExample;
+import skillter.advanceddatastructuers.VotingSystem;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashmapExample.initHashmap("snack", 1);
+
+        VotingSystem.vote("Skillter");
+        System.out.println(VotingSystem.getVoteList());
+        VotingSystem.vote("Skillter");
+        System.out.println(VotingSystem.getVoteList());
     }
 }
