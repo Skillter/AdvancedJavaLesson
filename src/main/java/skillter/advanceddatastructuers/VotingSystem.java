17c3c0e325b3d23eb1966ff8b4d5efb03a473048
Skillter <57605954+Skillter@users.noreply.github.com>
2023-02-12 12:24:17 +0100

    Update VotingSystem.java

--- src/main/java/skillter/advanceddatastructuers/VotingSystem.java
+++ src/main/java/skillter/advanceddatastructuers/VotingSystem.java
@@ -1,5 +1,6 @@
 package skillter.advanceddatastructuers;
 
+import java.util.Dictionary;
 import java.util.LinkedHashMap;
 import java.util.function.BiFunction;
 
@@ -8,9 +9,9 @@ public class VotingSystem {
     // Create a LinkedHashMap here, with the contestant name as the key
     // and the number of votes as the value.
 
-    public static LinkedHashMap<String, Integer> voteList = new LinkedHashMap<>();
+    public LinkedHashMap<String, Integer> voteList = new LinkedHashMap<>();
 
-    public static void vote(String contestant) {
+    public void vote(String contestant) {
         // Implement this method so that a new contestant is added to the map if it's
         // not already there. If it is already there, increment the value by one.
         voteList.merge(contestant, 1, Integer::sum);
@@ -18,7 +19,7 @@ public class VotingSystem {
 
     // This method should return the LinkedHashMap field you create at the top of
     // the class.
-    public static LinkedHashMap getVoteList() {
+    public LinkedHashMap getVoteList() {
         return voteList;
     }
 
